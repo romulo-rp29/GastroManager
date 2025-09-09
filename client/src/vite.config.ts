f21c769c70d1b25a106ac41f4f2c8ef9595d2ceb
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-09-08 22:35:16 -0300

    update vite config

--- client/src/vite.config.ts
+++ client/src/vite.config.ts
@@ -3,13 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname),
+  root: path.resolve(__dirname, ".."),
   plugins: [react()],
   css: {
     postcss: path.resolve(__dirname, "postcss.config.js"),
   },
   build: {
-    outDir: path.resolve(__dirname, "../dist"), // ou "build" se preferir
+    outDir: path.resolve(__dirname, "../dist"),
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "../index.html"),
+    },
+  },
+  server: {
+    port: 3000,
   },
 });
