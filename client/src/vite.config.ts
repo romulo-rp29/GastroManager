fd5d9af900415153d8f2732e6bfb32d7ba927329
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-09-08 22:55:24 -0300

    update package json

--- client/src/vite.config.ts
+++ client/src/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"), // define a pasta client como root
+  root: path.resolve(__dirname, 'client'),
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "dist/client"), // saída do build na raiz do projeto
-    emptyOutDir: true,
+    outDir: path.resolve(__dirname, 'dist/client')
   },
   server: {
     port: 5173,
