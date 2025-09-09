8e2d44fd75243ccd9e55f54067be832f240cc654
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-09-08 23:28:01 -0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve('client'),
+  root: path.resolve('client'), // se seu código React está dentro da pasta 'client'
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client/src'), // <- aqui é o alias para o @
+      '@': path.resolve(__dirname, 'client/src'),
+      '@shared': path.resolve(__dirname, 'client/src/shared'), // <- adicione aqui todos os aliases que você usa
     },
   },
   build: {
