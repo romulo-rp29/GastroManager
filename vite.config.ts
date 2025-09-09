bd2e10eba3a4245f0b04c47a0095e23581ccf3ee
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-09-08 22:22:40 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
-const __dirname = path.dirname(new URL('.', import.meta.url).pathname);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-  ],
+  root: path.resolve(__dirname, "client"), // aponta para a pasta client
   css: {
-    postcss: './postcss.config.cjs',
+    postcss: path.resolve(__dirname, "client/postcss.config.cjs"),
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
-  },
+  plugins: [react()],
 });
