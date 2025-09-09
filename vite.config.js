91684b6dc28338a1129f4e15f0690c639a732041
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-09-08 23:24:47 -0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'client'), // pasta do frontend
+  root: path.resolve('client'),
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src'), // <- aqui é o alias para o @
+    },
+  },
   build: {
     outDir: path.resolve(__dirname, 'dist/client'),
-    emptyOutDir: true
-  }
-})
+    emptyOutDir: true,
+  },
+});
